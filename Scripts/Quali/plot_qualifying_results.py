ef927a739d642e2919e9459b4dd5b14f3fcb5bf4
MihaiM21 <mihai.marinescu2112@yahoo.com>
2024-07-23 17:35:46 +0300

    Quali update

--- Scripts/Quali/plot_qualifying_results.py
+++ Scripts/Quali/plot_qualifying_results.py
@@ -1,10 +1,3 @@
-"""Qualifying results overview
-==============================
-
-Plot the qualifying result with visualization the fastest times.
-"""
-
-
 import matplotlib.pyplot as plt
 import pandas as pd
 from timple.timedelta import strftimedelta
@@ -12,29 +5,21 @@ from timple.timedelta import strftimedelta
 import fastf1
 import fastf1.plotting
 from fastf1.core import Laps
-
+import dirOrg
 
 
 def QualiResults(y,r,e):
 
-    # we only want support for timedelta plotting in this example
     fastf1.plotting.setup_mpl(mpl_timedelta_support=True, color_scheme=None, misc_mpl_mods=False)
 
     session = fastf1.get_session(y, r, e)
     session.load()
 
 
-    ##############################################################################
-    # First, we need to get an array of all drivers.
-
     drivers = pd.unique(session.laps['Driver'])
     print(drivers)
 
 
-    ##############################################################################
-    # After that we'll get each drivers fastest lap, create a new laps object
-    # from these laps, sort them by lap time and have pandas reindex them to
-    # number them nicely by starting position.
 
     list_fastest_laps = list()
     for drv in drivers:
@@ -43,33 +28,20 @@ def QualiResults(y,r,e):
     fastest_laps = Laps(list_fastest_laps).sort_values(by='LapTime').reset_index(drop=True)
 
 
-    ##############################################################################
-    # The plot is nicer to look at and more easily understandable if we just plot
-    # the time differences. Therefore we subtract the fastest lap time from all
-    # other lap times.
-
     pole_lap = fastest_laps.pick_fastest()
     fastest_laps['LapTimeDelta'] = fastest_laps['LapTime'] - pole_lap['LapTime']
 
 
-    ##############################################################################
-    # We can take a quick look at the laps we have to check if everything
-    # looks all right. For this, we'll just check the 'Driver', 'LapTime'
-    # and 'LapTimeDelta' columns.
-
     print(fastest_laps[['Driver', 'LapTime', 'LapTimeDelta']])
 
 
-    ##############################################################################
-    # Finally, we'll create a list of team colors per lap to color our plot.
     team_colors = list()
     for index, lap in fastest_laps.iterlaps():
         color = fastf1.plotting.team_color(lap['Team'])
         team_colors.append(color)
 
 
-    ##############################################################################
-    # Now, we can plot all the data
+
     fig, ax = plt.subplots()
     ax.barh(fastest_laps.index, fastest_laps['LapTimeDelta'],
         color=team_colors, edgecolor='grey')
@@ -85,18 +57,15 @@ def QualiResults(y,r,e):
     # sphinx_gallery_defer_figures
 
 
-    
-
-    ##############################################################################
-    # Finally, give the plot a meaningful title
 
     lap_time_string = strftimedelta(pole_lap['LapTime'], '%m:%s.%ms')
 
     plt.suptitle(f"{session.event['EventName']} {session.event.year} Qualifying\n"
              f"Fastest Lap: {lap_time_string} ({pole_lap['Driver']})")
 
-    plt.savefig('Quali results')
+    dirOrg.checkForFolder(session.event['EventName'])
+    plt.savefig("plots/" + session.event['EventName'] + "/" + 'Quali results.png')
 
     plt.show()
 
-#QualiResults(2023,22,'Q')
+    # working with program.py
