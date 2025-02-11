ef0325b1794002da0fde6c115ff800b34f077201
MihaiM21 <mihai.marinescu2112@yahoo.com>
2025-02-11 18:10:03 +0200

    Updated year list

--- program.py
+++ program.py
@@ -133,7 +133,7 @@ customtkinter.set_appearance_mode("dark")
 # Setting the current year and making a list of years to choose from 2000 till present
 year_list = []
 x = 2023
-while x <= 2024:
+while x <= 2025:
     year_list.append(str(x))
     x = x + 1
 
