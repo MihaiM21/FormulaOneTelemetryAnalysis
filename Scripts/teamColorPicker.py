c1bbf3712d8b4d22404123a9d6f0f7ea85a7a8f4
mihai <mihai.marinescu2112@yahoo.com>
2025-03-21 17:39:59 +0200

    Added driver colors

--- Scripts/teamColorPicker.py
+++ Scripts/teamColorPicker.py
@@ -44,10 +44,73 @@ def get_team_color(team):
         "Williams": "#64C4FF",
     }
 
+
+
     team = team.lower().strip()  # Normalizează numele echipei (ignora case sensitivity și spațiile)
 
     for official_name, aliases in team_aliases.items():
         if team in aliases:
             return team_colors[official_name]
 
-    return "#FFFFFF"  # Default: alb dacă echipa nu este găsită
+    return "#FFFFFF"
+
+
+def get_driver_color(driver):
+    driver_aliases = {
+        "Hamilton": ["HAM", "Hamilton"],
+        "Leclerc": ["LEC", "Leclerc"],
+        "Verstappen": ["VER", "Verstappen"],
+        "Lawson": ["LAW", "Lawson"],
+        "Russell": ["RUS", "Russell"],
+        "Antonelli": ["ANT", "Antonelli"],
+        "Norris": ["NOR", "Norris"],
+        "Piastri": ["PIA", "Piastri"],
+        "Stroll": ["STR", "Stroll"],
+        "Alonso": ["ALO", "Alonso"],
+        "Hulkenberg": ["HUL", "Hulkenberg"],
+        "Bortoleto": ["BOR", "Bortoleto"],
+        "Tsunoda": ["TSU", "Tsunoda"],
+        "Hadjar": ["HAD", "Hadjar"],
+        "Ocon": ["OCO", "Ocon"],
+        "Bearman": ["BEA", "Bearman"],
+        "Gasly": ["GAS", "Gasly"],
+        "Doohan": ["DOO", "Doohan"],
+        "Albon": ["ALB", "Albon"],
+        "Sainz": ["SAI", "Sainz"]
+    }
+
+
+    driver_colors = {
+        "Hamilton": "#E80020",
+        "Leclerc": "#b0041d",
+        "Verstappen": "#3671C6",
+        "Lawson": "#1e63c7",
+        "Russell": "#27F4D2",
+        "Antonelli": "#0dbda0",
+        "Norris": "#FF8000",
+        "Piastri": "#cc6804",
+        "Stroll": "#229971",
+        "Alonso": "#165c44",
+        "Hulkenberg": "#52E252",
+        "Bortoleto": "#2fb52f",
+        "Tsunoda": "#6692FF",
+        "Hadjar": "#4161b0",
+        "Ocon": "#B6BABD",
+        "Bearman": "#898c8f",
+        "Gasly": "#0093CC",
+        "Doohan": "#026d96",
+        "Albon": "#64C4FF",
+        "Sainz": "#387ca6"
+    }
+
+    driver = driver.lower().strip()
+    # print(f"Driver search: '{driver}'")
+
+    for official_name, aliases in driver_aliases.items():
+        # print(f"Checking {official_name}: {aliases}")
+        if driver in [alias.lower() for alias in aliases]:
+            # print(f"Found match: {official_name}, returning {driver_colors[official_name]}")
+            return driver_colors[official_name]
+
+    # print("No match found, returning #FFFFFF")
+    return "#FFFFFF"
