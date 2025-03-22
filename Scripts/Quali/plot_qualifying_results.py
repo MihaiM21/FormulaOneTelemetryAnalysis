8d6d642aa0c4d44199bc396111fdcf837ba76137
mihai <mihai.marinescu2112@yahoo.com>
2025-03-22 14:04:51 +0200

    Updated Plot with time gaps

--- Scripts/Quali/plot_qualifying_results.py
+++ Scripts/Quali/plot_qualifying_results.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 from timple.timedelta import strftimedelta
-
+import matplotlib.image as mpimg
 import fastf1
 import fastf1.plotting
 from fastf1.core import Laps
+from ..teamColorPicker import get_team_color
 import dirOrg
 
 
@@ -38,16 +39,35 @@ def QualiResults(y,r,e):
 
     team_colors = list()
     for index, lap in fastest_laps.iterlaps():
-        color = fastf1.plotting.team_color(lap['Team'])
+        # Removed FF1 color and using ours
+        # color = fastf1.plotting.team_color(lap['Team'])
+        color = get_team_color(lap['Team'])
         team_colors.append(color)
 
 
 
-    fig, ax = plt.subplots(figsize=(10, 10))
+    fig, ax = plt.subplots(figsize=(13, 13))
     ax.barh(fastest_laps.index, fastest_laps['LapTimeDelta'],
         color=team_colors, edgecolor='grey')
     ax.set_yticks(fastest_laps.index)
     ax.set_yticklabels(fastest_laps['Driver'])
+    max_value = fastest_laps['LapTimeDelta'].max()
+    ax.set_xlim(0, max_value * 1.15)
+
+
+    # Adding time gaps on the plot
+    fastest_lap_time = fastest_laps['LapTime'].min()
+    for i, lap in fastest_laps.iterrows():
+        lap_time = strftimedelta(lap['LapTime'], '%S.%ms')
+        pole_time = strftimedelta(fastest_lap_time, '%S.%ms')
+        pole_time_full = strftimedelta(fastest_lap_time, '%m:%s.%ms')
+        time_difference = abs(round(float(pole_time) - float(lap_time), 3))
+        if i==0:
+            ax.text(lap['LapTimeDelta'], i, f" {pole_time_full}s", va='center', fontsize=13, weight='bold')
+        else:
+            ax.text(lap['LapTimeDelta'], i, f" +{time_difference}s", va='center', fontsize=13, weight='bold')
+
+
 
     # show fastest at the top
     ax.invert_yaxis()
@@ -63,6 +83,10 @@ def QualiResults(y,r,e):
 
     plt.suptitle(f"{session.event['EventName']} {session.event.year} Qualifying\n"
              f"Fastest Lap: {lap_time_string} ({pole_lap['Driver']})")
+
+    # Adding Watermark
+    logo = mpimg.imread('lib/logo mic.png')
+    fig.figimage(logo, 575, 575, zorder=3, alpha=.6)
 
     dirOrg.checkForFolder(str(y) + "/" + session.event['EventName'])
     location = "plots/" + str(y) + "/" + session.event['EventName']
