f6655bf68961028bd5c9a1d2ac6088c690d1c597
mihai <mihai.marinescu2112@yahoo.com>
2025-03-23 21:48:34 +0200

    Watermark added and Color changes

--- Scripts/Quali/plot_speed_traces.py
+++ Scripts/Quali/plot_speed_traces.py
@@ -6,7 +6,9 @@ Compare two fastest laps by overlaying their speed traces.
 
 import matplotlib.pyplot as plt
 import fastf1.plotting
+import matplotlib.image as mpimg
 import dirOrg
+from ..teamColorPicker import get_driver_color, get_team_color
 
 
 def SpeedTraceFunc(y,r,e,d1,d2,t1,t2):
@@ -22,31 +24,35 @@ def SpeedTraceFunc(y,r,e,d1,d2,t1,t2):
     ##############################################################################
     # First, we select the two laps that we want to compare
 
-    ver_lap = session.laps.pick_driver(d1).pick_fastest()
-    ham_lap = session.laps.pick_driver(d2).pick_fastest()
+    driver1_lap = session.laps.pick_driver(d1).pick_fastest()
+    driver2_lap = session.laps.pick_driver(d2).pick_fastest()
 
     ##############################################################################
     # Next we get the telemetry data for each lap. We also add a 'Distance' column
     # to the telemetry dataframe as this makes it easier to compare the laps.
 
-    ver_tel = ver_lap.get_car_data().add_distance()
-    ham_tel = ham_lap.get_car_data().add_distance()
+    driver1_tel = driver1_lap.get_car_data().add_distance()
+    driver2_tel = driver2_lap.get_car_data().add_distance()
 
     ##############################################################################
     # Finally, we create a plot and plot both speed traces.
     # We color the individual lines with the driver's team colors.
 
-    rbr_color = fastf1.plotting.team_color(t1)
-    mer_color = fastf1.plotting.team_color(t2)
+    driver1_color = get_driver_color(d1)
+    driver2_color = get_driver_color(d2)
 
-    fig, ax = plt.subplots()
-    ax.plot(ver_tel['Distance'], ver_tel['Speed'], color=rbr_color, label=d1)
-    ax.plot(ham_tel['Distance'], ham_tel['Speed'], color=mer_color, label=d2)
+    fig, ax = plt.subplots(figsize=(13, 13))
+    ax.plot(driver1_tel['Distance'], driver1_tel['Speed'], color=driver1_color, label=d1)
+    ax.plot(driver2_tel['Distance'], driver2_tel['Speed'], color=driver2_color, label=d2)
 
     ax.set_xlabel('Distance in m')
     ax.set_ylabel('Speed in km/h')
 
     ax.legend()
+
+    # Adding Watermark
+    logo = mpimg.imread('lib/logo mic.png')
+    fig.figimage(logo, 575, 575, zorder=3, alpha=.6)
 
     plt.suptitle('Fastest Lap Comparison\n' + str(y) + " " + session.event['EventName'] + ' ' + session.name)
 
